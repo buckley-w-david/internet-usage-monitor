02cea94849d85d8af10fd32b27ae56828e41a9bf
David Buckley <buckley.w.david@gmail.com>
2019-04-23 13:07:04 -0400

    remove redundant import

--- usage_monitor/check.py
+++ usage_monitor/check.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
-
 
 LOGIN_PAGE='https://xplornet.force.com/customers/loginCommunity'
 
